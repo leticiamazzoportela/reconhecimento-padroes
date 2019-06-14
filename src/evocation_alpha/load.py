51ab94efd1775a64bd4e27a8d1df5692e7f5a626
Letícia Mazzo Portela <leticiaportela@alunos.utfpr.edu.br>
2019-06-14 15:16:16 -0300

    Convertendo array

--- src/evocation_alpha/load.py
+++ src/evocation_alpha/load.py
@@ -1,3 +1,8 @@
+# %matplotlib inline
+
+# import matplotlib.pyplot as plt
+# import mne
+
 def load_data(file):
     with open(file, 'r') as f:
         data = f.readlines()
@@ -9,6 +14,18 @@ def load_data(file):
 
     return compactData
 
+def convert(data):
+    temp = []
+    line = data.split(',')
+
+    for e in range(len(line)):
+        if e <= 6:
+            temp.append(float(line[e]))
+        if e == 12:
+            temp.append(line[e].split('\n')[0].strip())
+    
+    return temp
+
 def separate_data(data):
     index = []
     temp = []
@@ -17,17 +34,34 @@ def separate_data(data):
     for e in range(len(data)):
         idx = data[e].split(',')[0]
         if int(idx) == 255 and count == 255:
-            index.append(temp)
+            index.extend(temp)
             temp = []
             count = 0
         elif int(idx) == 255 and count < 255:
             temp = []
             count = 0
         else:
-            temp.append(data[e])
+            temp.append(convert(data[e]))
             count += 1
     
     return index
     
 data = load_data('dataset_alpha/RAW_00.txt')
 newData = separate_data(data)
+
+# def sem_nome(data):
+#     newData = np.asarray(data)
+    
+#     ch_names = ['PO3', 'PO4', 'P8', 'O1', 'O2', 'P7']
+#     ch_types = ['eeg'] * 6
+
+#     info = mne.create_info(ch_names=ch_names, sfreq=256, ch_types=ch_types)
+#     raw = mne.io.RawArray(newData, info)
+
+#     plt.plot(np.linspace(0, len(data), len(data)*256), raw.get_data()[0]) #eletrodo index 0
+#     plt.xlabel('tempo (s)')
+#     plt.ylabel('Dados EEG (mV/cm²)')
+
+#     raw.plot_psd
+
+# sem_nome(newData)
